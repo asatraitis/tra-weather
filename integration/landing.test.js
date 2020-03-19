f82a9f4916beecf35da548aa7e7411b51efd6122
Andy <asatraitis@allegisgroup.com>
2020-03-19 11:47:25 -0400

    jest-puppeteer config; first test

---
+++ integration/landing.test.js
@@ -0,0 +1,17 @@
+describe('App', () => {
+    beforeAll(async () => {
+        await page.goto('http://localhost:3000')
+    })
+    it('Should display logo', async () => {        
+        await expect(page).toMatch('TraWeather')
+    })
+    it('Should see click area for "new location"', async () => {
+        await expect(page).toMatchElement('.add-init', { text: 'Click to Add a Location' })
+    })
+    it('Should click "new location"', async () => {        
+        await expect(page).toClick('.add-init') 
+    })
+    it('Should see form modal', async () => {
+        await expect(page).toMatchElement('.form-modal.shown');
+    })
+})
